f7683e93fc19aadb723f477a7bba8483fab5d0af
Vicente Pastor <persiafighter@gmail.com>
2020-06-14 13:30:22 +0200

    Add support for custom query identifiers

--- MySqlConnectorWrapper/Caching/CacheManager.cs
+++ MySqlConnectorWrapper/Caching/CacheManager.cs
@@ -51,8 +51,7 @@ namespace Pustalorc.Libraries.MySqlConnectorWrapper.Caching
         /// <returns>The cache item if it is found or null otherwise.</returns>
         public QueryOutput GetItemInCache(Query query)
         {
-            var cachedQuery = _cache.FirstOrDefault(k =>
-                k?.Query.QueryString.Equals(query.QueryString, StringComparison.OrdinalIgnoreCase) == true);
+            var cachedQuery = _cache.FirstOrDefault(k => k?.Query.Identifier == query.Identifier);
 
             if (cachedQuery == null)
                 return null;
@@ -86,8 +85,7 @@ namespace Pustalorc.Libraries.MySqlConnectorWrapper.Caching
 
             return first == null
                 ? _cache.FindFirstIndexNull()
-                : _cache.FindFirstIndex(k =>
-                    k?.Query.QueryString.Equals(first.Query.QueryString, StringComparison.OrdinalIgnoreCase) == true);
+                : _cache.FindFirstIndex(k => k?.Query.Identifier == first.Query.Identifier);
         }
 
         private void UpdateCacheItems(object sender, ElapsedEventArgs e)
