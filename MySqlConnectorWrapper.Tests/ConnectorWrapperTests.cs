8769bb82189058fb2a2bc8e331246925c3daa311
Vicente Pastor <persiafighter@gmail.com>
2020-06-14 13:30:48 +0200

    Add a couple more tests and fix some of the old ones
    I will add more later on

--- MySqlConnectorWrapper.Tests/ConnectorWrapperTests.cs
+++ MySqlConnectorWrapper.Tests/ConnectorWrapperTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pustalorc.Libraries.MySqlConnectorWrapper.Queries;
 
@@ -10,20 +11,49 @@ namespace Pustalorc.Libraries.MySqlConnectorWrapper.Tests
         private readonly Database _database = new Database();
 
         [TestMethod]
+        public void CreateDropTableTest()
+        {
+            var output = _database.ExecuteQuery(new Query(0, "CREATE TABLE `test` (`id` VARCHAR(5) NOT NULL);",
+                EQueryType.NonQuery));
+
+            Assert.IsTrue(int.TryParse(output.Output?.ToString(), out var result) && result >= 0);
+
+            output = _database.ExecuteQuery(new Query(0, "DROP TABLE `test`;", EQueryType.NonQuery));
+
+            Assert.IsTrue(int.TryParse(output.Output?.ToString(), out result) && result >= 0);
+        }
+
+        [TestMethod]
         public void ExecuteQueryTest()
         {
-            var output = _database.ExecuteQuery(new Query("SHOW TABLES LIKE 'test';", EQueryType.Scalar));
-            Assert.IsNull(output.Output);
+            _database.ExecuteQuery(new Query(0, "CREATE TABLE `test` (`id` VARCHAR(5) NOT NULL);",
+                EQueryType.NonQuery));
+
+            var output = _database.ExecuteQuery(new Query(1, "SHOW TABLES LIKE 'test';", EQueryType.Scalar));
+
+            _database.ExecuteQuery(new Query(0, "DROP TABLE `test`;", EQueryType.NonQuery));
+
+            Assert.IsTrue("test".Equals(output.Output.ToString(), StringComparison.OrdinalIgnoreCase));
         }
 
         [TestMethod]
         public void ExecuteTransactionTest()
         {
-            var allOut = _database.ExecuteTransaction(new Query("SHOW TABLES LIKE 'test';", EQueryType.Scalar),
-                new Query("SHOW TABLES LIKE 'test2';", EQueryType.Scalar),
-                new Query("SHOW TABLES LIKE 'test3';", EQueryType.Scalar));
+            _database.ExecuteTransaction(
+                new Query(4, "CREATE TABLE `test` (`id` VARCHAR(5) NOT NULL);", EQueryType.NonQuery),
+                new Query(5, "CREATE TABLE `test2` (`id` VARCHAR(5) NOT NULL);", EQueryType.NonQuery),
+                new Query(6, "CREATE TABLE `test3` (`id` VARCHAR(5) NOT NULL);", EQueryType.NonQuery));
 
-            Assert.IsFalse(allOut.Any(k => k.Output != null));
+            var allOut = _database.ExecuteTransaction(new Query(1, "SHOW TABLES LIKE 'test';", EQueryType.Scalar),
+                new Query(2, "SHOW TABLES LIKE 'test2';", EQueryType.Scalar),
+                new Query(3, "SHOW TABLES LIKE 'test3';", EQueryType.Scalar));
+
+
+            _database.ExecuteTransaction(new Query(4, "DROP TABLE `test`;", EQueryType.NonQuery),
+                new Query(5, "DROP TABLE `test2`;", EQueryType.NonQuery),
+                new Query(6, "DROP TABLE `test3`;", EQueryType.NonQuery));
+
+            Assert.IsTrue(allOut.All(k => k.Output != null));
         }
     }
 }
